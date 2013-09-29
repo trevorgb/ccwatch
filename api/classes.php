a86316e882fff246aae64e817f1fe0aa8611d135
Trevor Merritt - Geekback <trevor.merritt@geekback.com>
2013-09-29 12:50:28 -0400

    added another exchange - BTC-e added
    i've mangled the sample code to make BTC-E and cryptsy APIs read now.  I need to DRY them out.

--- api/classes.php
+++ api/classes.php
@@ -94,11 +94,66 @@ class market {
 
 
 class exchange {
-   public static function getInfo($key, $secret) {
-      return self::apiquery('getinfo', $key, $secret);
+   public static function getInfo($key, $secret, $type) {
+    
+      switch($type) {
+      case 'cryptsy':
+        $vals = self::cryptsyquery('getinfo', $key, $secret);
+        if ($vals['success'] == 1) {
+            $vals = $vals['return']['balances_available'];
+        }
+        break;
+      case 'BTCE':
+        $vals = self::btcequery('getInfo', $key, $secret);
+        $vals = $vals['return']['funds'];
+      }
+      $balances = array();
+      foreach($vals as $c=>$b) {
+        if ($b > 0) {
+            $balances[$c] = $b;
+        }
+      }
+      return $balances;
    }
    
-   private static function apiquery($method, $key, $secret, array $req = array()) {
+    private static function btcequery($method, $key, $secret) {
+            // API settings
+      $req['method'] = $method;
+      $mt = explode(' ', microtime());
+      $req['nonce'] = $mt[1];
+      
+      // generate the POST data string
+      $post_data = http_build_query($req, '', '&');
+   
+      $sign = hash_hmac("sha512", $post_data, $secret);
+   
+      // generate the extra headers
+      $headers = array(
+         'Sign: '.$sign,
+         'Key: '.$key,
+      );
+   
+      // our curl handle (initialize if required)
+      static $ch = null;
+      if (is_null($ch)) {
+         $ch = curl_init();
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; BTCE PHP client; '.php_uname('s').'; PHP/'.phpversion().')');
+      }
+      curl_setopt($ch, CURLOPT_URL, 'https://btc-e.com/tapi/');
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+   
+      // run the query
+      $res = curl_exec($ch);
+      if ($res === false) throw new Exception('Could not get reply: '.curl_error($ch));
+      $dec = json_decode($res, true);
+      if (!$dec) throw new Exception('Invalid data received, please make sure connection is working and requested API exists');
+      return $dec;
+    }
+   
+   private static function cryptsyquery($method, $key, $secret, array $req = array()) {
         // API settings
         $req['method'] = $method;
         $mt = explode(' ', microtime());
